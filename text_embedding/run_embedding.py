1a85e3d91bdc6909dca32a3712c6adde43fee66c
Archai222 <40301843+Archai222@users.noreply.github.com>
2018-07-31 16:33:59 -0700

    improving run_embedding

--- text_embedding/run_embedding.py
+++ text_embedding/run_embedding.py
@@ -1,4 +1,12 @@
 from text_embedding import documents
+from text_embedding import cooc
+from text_embedding.documents import evaluate
+from text_embedding.documents import TASKMAP
 
 if __name__ == '__main__':
-    print('hi')
+    represent, prepare, invariant = cooc.DisC(1, 'mult')
+
+    print(represent)
+    print(prepare)
+    evaluate('mr', represent, prepare=prepare, invariant=invariant, verbose=True,
+             intercept='mr' in TASKMAP['pairwise task'])
